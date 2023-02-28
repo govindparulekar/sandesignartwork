25eab6cba45594b611d9f4d090d7861075b5f3a9
Govind <devgovind511@gmail.com>
2023-03-01 00:19:34 +0530

    Intro section added

---
+++ components/main.js
@@ -0,0 +1,14 @@
+import styles from '../styles/Main.module.css'
+
+export default function Main() {
+    return(
+        <div id={styles.intro_section}>
+            <div id={styles.heading}>
+                Lorem aaaksjdflkjj aslkdjf lkaj lksdj f
+            </div>
+            <div id={styles.desc}>
+            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letr
+            </div>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from '../components/header'
+import Main from '../components/main'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,12 +19,14 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap" rel="stylesheet"></link>
         <script async src="https://kit.fontawesome.com/4e203ce1b2.js" crossorigin="anonymous"></script>
       </Head>
       
       <main className={styles.main}>
         <div class="container">
               <Header/>
+              <Main/>
           </div>
       </main>
     </>
