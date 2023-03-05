df7a23e154ae5f555ac63803431a77373b2f52e9
Govind <devgovind511@gmail.com>
2023-03-05 18:56:06 +0530

    sfasdf

--- components/main.js
+++ components/main.js
@@ -2,6 +2,7 @@ import styles from '../styles/Main.module.css'
 
 export default function Main() {
     return(
+        <>
         <div id={styles.intro_section}>
             <div id={styles.heading}>
                 Lorem aaaksjdflkjj aslkdjf lkaj lksdj f
@@ -10,5 +11,11 @@ export default function Main() {
             ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letr
             </div>
         </div>
+
+        <div id={styles.demo_work}>
+          
+            
+        </div>
+        </>
     );
 }
