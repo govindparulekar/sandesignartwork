33f48c6de1beea6bd280609eb43a0197f9bc7dd9
Govind <68502881+govindparulekar@users.noreply.github.com>
2023-03-06 20:21:39 +0530

    test update for vercel

--- components/main.js
+++ components/main.js
@@ -5,7 +5,7 @@ export default function Main() {
         <>
         <div id={styles.intro_section}>
             <div id={styles.heading}>
-                Lorem aaaksjdflkjj aslkdjf lkaj lksdj f
+                when an unknown printer took a galley of type an
             </div>
             <div id={styles.desc}>
             ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letr
